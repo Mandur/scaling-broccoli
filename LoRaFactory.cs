4b995371381711d2057cb1c483b5ad7c9adcd4d9
Mikhail <mikhail.chatillon@hotmail.com>
2018-09-08 15:55:34 +0200

    initial commit

---
+++ LoRaFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoProject
+{
+
+    public static class LoRaFactory{
+
+        public static GenericLora create(bool _isOne)
+        {
+            if (_isOne)
+                return new LoRaImplem1(1);
+            else
+                return new LoRaImplem2();
+        }
+    }
+
+
+    public abstract class GenericLora
+    {
+    
+        //common properties and methods
+        public int DemoField { get; set; }
+        public string GetDemo()
+        {
+            return "it works";
+        }
+
+    }
+    class LoRaImplem1 : GenericLora
+    {
+        //specific field
+        public int HowDoIAccessMe { get; set; }
+
+        public LoRaImplem1(int _howDoIAccessMe)
+        {
+            HowDoIAccessMe = _howDoIAccessMe;
+        }
+    }
+
+    class LoRaImplem2 : GenericLora
+    {
+        //No specific field here
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DemoProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var result = LoRaFactory.create(true);
+            //How do I access result.HowDoIAccessMe without a cast now?
+        }
+    }
+}
